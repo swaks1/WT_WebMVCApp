044b4165f84a0c7fd12445bbc05ae2683ef80886
Riste Poposki <rickyarpy@hotmail.com>
2018-05-26 16:27:07 +0200

    Add project files.

---
+++ WT_WebMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WT_WebMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
